8057185c7efdfabbbd436c06f5f653eb58c2ccb4
Sastiazaran <sastiazaran2000@gmail.com>
2023-05-21 19:21:28 -0600

    funcionalidad de ionic implementada

--- proyecto/src/App.vue
+++ proyecto/src/App.vue
@@ -6,4 +6,25 @@
 
 <script setup>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth, "sastiazaran@hornymail.com",
+"123456")
+.then((userCredential) => {
+// Signed in
+const user = userCredential.user;
+if(user== null)
+{
+console.log("no autenticado") }
+else
+{
+console.log("usuario autenticado", user);
+}
+})
+.catch((error) => {
+const errorCode = error.code;
+const errorMessage = error.message;
+console.log(errorCode, errorMessage);
+});
 </script>
